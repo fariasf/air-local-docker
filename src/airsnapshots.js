1d25af2e76cf1271fe81461bf82c69626e668f15
RiFi2k <luckylowery@gmail.com>
2019-06-11 18:14:57 -0400

    Airsnapshots with auth cli command

--- src/airsnapshots.js
+++ src/airsnapshots.js
@@ -1,67 +1,60 @@
 const chalk = require('chalk')
 const commandUtils = require('./command-utils')
 const fs = require('fs-extra')
-const path = require('path')
-const execSync = require('child_process').execSync
-const envUtils = require('./env-utils')
-const gateway = require('./gateway')
 const configure = require('./configure')
+const auth = require('./auth')
+
+const help = function () {
+  let help = `
+Usage: airlocal airsnapshots
+
+Commands:
+  create        Create a new DB snapshot
+`
+  console.log(help)
+}
 
 const getSnapshotsDir = async function () {
   return await configure.get('snapshotsPath')
 }
 
 const checkIfConfigured = async function () {
-  let airSnapshotsDir = await getSnapshotsDir()
+  const authConfigured = await auth.checkIfAuthConfigured()
 
-  if (await fs.exists(path.join(airSnapshotsDir, 'config.json')) === false) {
+  if (authConfigured === false) {
     return false
   }
 
   return true
 }
 
-const command = async function () {
-  // false catches the case when no subcommand is passed, and we just pass to snapshots to show usage
-  let bypassCommands = [ undefined, 'configure', 'help', 'list', 'create-repository' ]
-  let noPathCommands = [ undefined, 'configure', 'help', 'list', 'create-repository', 'delete', 'search', 'download' ]
-  let envPath = false
+const createSnapshot = function () {
+  console.log(chalk.yellow('Snapshots still WIP'))
+}
 
-  // Ensure that the wpsnapshots folder is created and owned by the current user versus letting docker create it so we can enforce proper ownership later
+const command = async function () {
+  // Ensure that the snapshots folder is created and owned by the current user versus letting docker create it so we can enforce proper ownership later
   let airSnapshotsDir = await getSnapshotsDir()
   await fs.ensureDir(airSnapshotsDir)
 
-  // Except for a few whitelisted commands, enforce a configuration before proceeding
-  if (bypassCommands.indexOf(commandUtils.subcommand()) === -1) {
-    // Verify we have a configuration
-    if (await checkIfConfigured() === false) {
-      console.error(chalk.red('Error: ') + "AirSnapshots does not have a configuration file. Please run 'airlocal airsnapshots configure' before continuing.")
-      process.exit()
-    }
+  // Verify we have a configuration
+  if (await checkIfConfigured() === false) {
+    console.error(chalk.red('Error: ') + "AIRSnapshots does not have auth configured. Please run 'airlocal auth config' before continuing.")
+    process.exit()
   }
 
-  // These commands can be run without being in the context of a WP install
-  if (noPathCommands.indexOf(commandUtils.subcommand()) === -1) {
-    let envSlug = await envUtils.parseOrPromptEnv()
-    if (envSlug === false) {
-      console.error(chalk.red('Error: ') + 'Unable to determine which environment to use AirSnapshots with. Please run this command from within your environment.')
-      process.exit(1)
+  if (commandUtils.subcommand() === 'help' || commandUtils.subcommand() === false) {
+    help()
+  } else {
+    switch (commandUtils.subcommand()) {
+      case 'create':
+        createSnapshot()
+        break
+      default:
+        help()
+        break
     }
-    envPath = await envUtils.envPath(envSlug)
   }
-
-  // Get everything after the snapshots command, so we can pass to the docker container
-  let command = commandUtils.commandArgs()
-
-  // @todo update the image version once new images are merged
-  try {
-    if (envPath === false) {
-      execSync(`docker run -it --rm -v "${wpsnapshotsDir}:/home/airsnapshots/.airsnapshots" 45air/airsnapshots:dev ${command}`, { stdio: 'inherit' })
-    } else {
-      await gateway.startGlobal()
-      execSync(`docker run -it --rm --network airlocaldocker -v "${envPath}/wordpress:/var/www/html" -v "${wpsnapshotsDir}:/home/airsnapshots/.airsnapshots" 45air/airsnapshots:dev --db_user=root ${command}`, { stdio: 'inherit' })
-    }
-  } catch (ex) {}
 }
 
 module.exports = { command, checkIfConfigured, configure }
